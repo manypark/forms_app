953a87caaceebaafc373fe57de5b8ed7b8aa8b61
Manu <manypark@live.com>
2023-06-06 20:26:21 -0600

    feat: new event reset and show transaction

--- lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -7,12 +7,19 @@ part 'counter_state.dart';
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
 
   CounterBloc() : super( const CounterState() ) {
+    on<CounterIncreased>( _onCounterIncreased );
+    on<CounterReset>( _onCounterReset );
+  }
 
-    on<CounterIncreased>((event, emit) {
-      emit( state.copyWith(
+  void _onCounterIncreased( CounterIncreased event, Emitter<CounterState> emit ) {
+    emit( state.copyWith(
         counter         : state.counter + event.value,
         transactionCount: state.transactionCount + 1,
-      ) );
-    });
+    ));
   }
+
+  void _onCounterReset( CounterReset event, Emitter<CounterState> emit ) {
+    emit( state.copyWith( counter: 10 ) );
+  }
+
 }
--- lib/presentation/screens/bloc_counter_screen.dart
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_bloc/counter_bloc.dart';
 
 class BlocCounterScreen extends StatelessWidget {
-
   const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (context) => CounterBloc(), child: const _BlocCounterView());
+  }
+}
+
+class _BlocCounterView extends StatelessWidget {
+  const _BlocCounterView();
+
+  void increaseCounterBy(BuildContext context, [int value = 1]) {
+    context.read<CounterBloc>().add(CounterIncreased(value));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bloc Counter'),
+        title: BlocBuilder<CounterBloc, CounterState>(
+          builder: (context, state) {
+            return Text('Bloc Counter: ${state.transactionCount}');
+          },
+        ),
         actions: [
           IconButton(
-            icon     : const Icon( Icons.refresh_outlined ),
-            onPressed: () => {},
+            icon: const Icon(Icons.refresh_outlined),
+            onPressed: () => context.read<CounterBloc>().add( CounterReset() ),
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Counter value: XXXX'),
+      body: Center(
+        child: context.select((CounterBloc counterBloc) => Text(
+            'Counter value: ${counterBloc.state.counter}',
+            style: const TextStyle(fontSize: 24))),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -25,31 +46,29 @@ class BlocCounterScreen extends StatelessWidget {
           FloatingActionButton(
             heroTag: '1',
             child: const Text('+3'),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context, 3),
           ),
-
-          const SizedBox( height: 15 ,),
-
+          const SizedBox(
+            height: 15,
+          ),
           FloatingActionButton(
             heroTag: '2',
             child: const Text('+2'),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context, 2),
           ),
-
-          const SizedBox( height: 15 ,),
-
+          const SizedBox(
+            height: 15,
+          ),
           FloatingActionButton(
             heroTag: '3',
             child: const Text('+1'),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context, 1),
           ),
-
-          const SizedBox( height: 15 ,),
-
+          const SizedBox(
+            height: 15,
+          ),
         ],
       ),
     );
-    
   }
-
 }
